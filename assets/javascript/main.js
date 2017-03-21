7a95b60add6f3084c09d640b93d2e0de3c36e1fe
Brad Goldsmith <brad.m.goldsmith@gmail.com>
2017-03-21 11:12:08 -0400

    styled the main page and started changes to the javascript.  Unfortunately doesn't work right but I'm sure it's a small syntax.  Definitely cleaning up code though

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -64,6 +64,65 @@ function geocodeAddress(geocoder, resultsMap) {
     });
 }
 
+function weatherSearch(city, state) {
+    var weather = {
+        "async": true,
+        "crossDomain": true,
+        "url": "http://api.wunderground.com/api/d04d4a5c28ba5311/conditions/q/" + state + "/" + city + ".json",
+        "method": "GET"
+    }
+
+    $.ajax(weather).done(function(response) {
+        console.log(response);
+        var weatherDiv = $('<div>').addClass('weatherDiv');
+        var weatherImg = $('<img>').addClass('weatherImg').attr('src', response.current_observation.icon_url);
+        weatherDiv.append(weatherImg).appendTo('.card-image');
+        var currentWeather = $('<div>').addClass('currentWeather');
+        var currentTemp = $('<p>').addClass('currTemp').text('temp:' + response.current_observation.temp_f);
+        var currentLocation = $('<p>').addClass('location').text(response.current_observation.display_location.city);
+        var time = $('<p>').addClass('localTime').text(response.current_observation.local_time);
+        var forcast = $('<p>').addClass('forcast').text('forcast:' + response.current_observation.weather);
+        currentWeather.append(currentLocation, currentTemp, forcast).appendTo(".card-content");
+
+
+    });
+
+}
+
+function activitySearch(city) {
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://trailapi-trailapi.p.mashape.com/?q%5Bcity_cont%5D=" + city + "&limit=10",
+        "method": "GET",
+        "headers": {
+            "x-mashape-key": "8EIXIQVdijmshJoTnDa6TFps8GArp1zwCjwjsn0ng4oTPC4UUR",
+            "accept": "text/plain",
+            "cache-control": "no-cache",
+            "postman-token": "4b9bc18d-b74a-c8c1-8e97-6d5bb32bb471"
+        }
+    }
+
+    $.ajax(settings).done(function(response) {
+        var carouselDiv = $('<div>').addClass('carousel');
+        for (var i = 0; i < 10; i++) {
+            if (response.places[i].activities[0]) {
+                //console.log(response.places[i].activities[0].thumbnail);
+                //console.log(response.places[i].activities[0].description);
+                var carouselImg = $('<img>').addClass('carouselImg')
+                    .attr('src', response.places[i].activities[0].thumbnail)
+                    .attr('data-type', response.places[i].activities[0].activity_type_name)
+                    .attr('data-description', response.places[i].activities[0].description);
+                var carouselAtag = $('<a>').addClass('carousel-item');
+                carouselAtag.append(carouselImg).appendTo(carouselDiv);
+            }
+        }
+        carouselDiv.appendTo('#carousel');
+    });
+
+}
+
+
 $(document).ready(function() {
 
 
@@ -87,8 +146,6 @@ $(document).ready(function() {
     }
 
 
-    // this is the api call for the trails
-
 
 
     $("#explore").on("click", function() {
@@ -99,72 +156,22 @@ $(document).ready(function() {
         var city = split[0];
         var state = split[1];
 
-        var settings = {
-            "async": true,
-            "crossDomain": true,
-            "url": "https://trailapi-trailapi.p.mashape.com/?q%5Bcity_cont%5D=" + city + "&limit=10",
-            "method": "GET",
-            "headers": {
-                "x-mashape-key": "8EIXIQVdijmshJoTnDa6TFps8GArp1zwCjwjsn0ng4oTPC4UUR",
-                "accept": "text/plain",
-                "cache-control": "no-cache",
-                "postman-token": "4b9bc18d-b74a-c8c1-8e97-6d5bb32bb471"
-            }
-        }
 
-        $.ajax(settings).done(function(response) {
-            var carouselDiv = $('<div>').addClass('carousel');
-            for (var i = 0; i < 10; i++) {
-                if (response.places[i].activities[0]) {
-                    //console.log(response.places[i].activities[0].thumbnail);
-                    //console.log(response.places[i].activities[0].description);
-                    var carouselImg = $('<img>').addClass('carouselImg')
-                        .attr('src', response.places[i].activities[0].thumbnail)
-                        .attr('data-type', response.places[i].activities[0].activity_type_name)
-                        .attr('data-description', response.places[i].activities[0].description);
-                    var carouselAtag = $('<a>').addClass('carousel-item');
-                    carouselAtag.append(carouselImg).appendTo(carouselDiv);
-                }
-            }
-            carouselDiv.appendTo('#carousel');
 
-            $('.carousel').carousel({ duration: 1000 });
-            $('.carousel').hover(stop, run);
+        $('.carousel').carousel({ duration: 1000 });
+        $('.carousel').hover(stop, run);
+        activitySearch(city);
+        weatherSearch(state, city);
 
 
 
-            var weather = {
-                "async": true,
-                "crossDomain": true,
-                "url": "http://api.wunderground.com/api/d04d4a5c28ba5311/conditions/q/" + state + "/" + city + ".json",
-                "method": "GET"
-            }
 
-            $.ajax(weather).done(function(response) {
-                console.log(response);
-                var weatherDiv = $('<div>').addClass('weatherDiv');
-                var weatherImg = $('<img>').addClass('weatherImg').attr('src', response.current_observation.icon_url);
-                weatherDiv.append(weatherImg).appendTo('.card-image');
-                var currentWeather = $('<div>').addClass('currentWeather');
-                var currentTemp = $('<p>').addClass('currTemp').text('temp:' + response.current_observation.temp_f);
-                var currentLocation = $('<p>').addClass('location').text(response.current_observation.display_location.city);
-                var time = $('<p>').addClass('localTime').text(response.current_observation.local_time);
-                var forcast = $('<p>').addClass('forcast').text('forcast:' + response.current_observation.weather);
-                currentWeather.append(currentLocation, currentTemp, forcast).appendTo(".card-content");
-
-
-            });
-        });
     });
+});
 
 
-    $("#carousel").on('click', ".carouselImg", function(event) {
-        var type = $(event.target).attr("data-type");
-        var description = $(event.target).attr('data-description')
-        $('#info').html(type + "<br><br>" + description);
-    });
-
-
-
-
+$("#carousel").on('click', ".carouselImg", function(event) {
+    var type = $(event.target).attr("data-type");
+    var description = $(event.target).attr('data-description')
+    $('#info').html(type + "<br><br>" + description);
 });
